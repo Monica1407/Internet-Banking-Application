a5c68a4c8a64d3da7c16fb2b7461b9bbe0b0db36
Monica1407 <106542911+Monica1407@users.noreply.github.com>
2023-05-12 13:57:30 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToMongoose = async () => {
   try {
-    const db = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://monicaseenu702:moni@1407@cluster0.cdfhrfr.mongodb.net/bank?retryWrites=true&w=majority', {
+    const db = await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://monicaseenu702:moni1407@cluster0.cdfhrfr.mongodb.net/bank?retryWrites=true&w=majority', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
